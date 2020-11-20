08533eee99297b4cb5179e90dd61ef5202e210eb
Grzegorz Klimaszewski <2612193+grzegorzk@users.noreply.github.com>
2020-11-21 00:07:49 +0100

    adding --login to invokation ot su

--- docker_files/entrypoint.sh
+++ docker_files/entrypoint.sh
@@ -9,4 +9,4 @@ cp -r -p /root/.config /home/ch/.config
 chown -R ch:ch /home/ch/.Xauthority
 
 # Read https://developers.google.com/web/updates/2017/04/headless-chrome - 'How do I create a Docker container that runs Headless Chrome?'
-su ch --command "chromium --no-sandbox"
+su --login ch --command "chromium --no-sandbox"
